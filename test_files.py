b97b31b549e0f1f2ec8f6456084120b0101de7f9
Matheus Nunes <mhnnunes@dcc.ufmg.br>
2018-11-04 09:38:05 -0200

    Add modularization to code, add multiple seeds testing

--- test_files.py
+++ test_files.py
@@ -15,13 +15,53 @@ from io_utils import parse_synthetic_dataset
 from io_utils import plot_clustering_results
 
 
-random_seed = 42
+def run_heuristic_on_dataset(func, X, k, h_name, filename, plots_dir, labels,
+                             seed, verbose):
+    dataset_name = filename.split('.')[0]
+    dataset_size = int(dataset_name.split('_')[1])
+    # BEGIN TEST: LLOYD HEURISTIC
+    before = default_timer()
+    try:
+        print('before calling', h_name)
+        clusters, ssq = func()
+        print('after calling', h_name)
+    except Exception as e:
+        print('EXCEPTION ON FILE', filename, e)
+        if verbose:
+            print(e)
+        return False, pd.DataFrame([[dataset_size,
+                                     h_name,
+                                     k,
+                                     np.nan,
+                                     np.nan]],
+                                   columns=['size',
+                                            'method',
+                                            'k',
+                                            'seed',
+                                            'time',
+                                            'ssq'])
+    else:
+        after = default_timer() - before
+        plot_clustering_results(X, clusters, h_name, labels,
+                                join(plots_dir, dataset_name +
+                                     '_k=' + str(k) + '_' + h_name + '.png'))
+        return True, pd.DataFrame([[dataset_size,
+                                    h_name,
+                                    k,
+                                    after,
+                                    ssq]],
+                                  columns=['size',
+                                           'method',
+                                           'k',
+                                           'seed',
+                                           'time',
+                                           'ssq'])
 
 
 def test_datasets(datadir, outdir, verbose=False):
     dataset_dir = join(getcwd(), datadir)
     # result_dir = join(getcwd(), outdir)
-    plots_dir = join(outdir, 'plots')
+    plots_dir = join(getcwd(), outdir, 'plots')
 
     for distribution in listdir(dataset_dir):
         # Test datasets generated using both distributions:
@@ -29,154 +69,36 @@ def test_datasets(datadir, outdir, verbose=False):
         for filename in sorted(listdir(join(dataset_dir, distribution))):
             # Read each test file
             if verbose:
-                print("=========== Starting test on file: ", filename, '============')
+                print("=========== Starting test on file: ",
+                      filename, '============')
             print('before reading data')
             raw_data = read_input(join(dataset_dir, distribution, filename))
             X, labels = parse_synthetic_dataset(raw_data)
             print('after reading data')
             # Define values for k and put it in a loop
-            results = pd.DataFrame(columns=['dataset', 'method', 'k',
+            results = pd.DataFrame(columns=['size', 'method', 'k', 'seed',
                                             'time', 'ssq'])
             for k in range(1, 11):
-                kmns = KMeans(X, random_seed, k, verbose)
-                # BEGIN TEST: LLOYD HEURISTIC
-                before = default_timer()
-                try:
-                    print('before calling lloyd')
-                    clusters_lloyd, ssq_lloyd = kmns.lloyd_heuristic()
-                    print('after calling lloyd')
-                except Exception as e:
-                    print('EXCEPTION ON FILE', filename, e)
-                    if verbose:
-                        print(e)
-                    results = \
-                        pd.concat([results, pd.DataFrame([[filename,
-                                                           'lloyd', k,
-                                                           np.nan, np.nan]],
-                                                         columns=['dataset',
-                                                                  'method',
-                                                                  'k',
-                                                                  'time',
-                                                                  'ssq'])],
-                                  axis=0)
-                    continue
-                else:
-                    after = default_timer() - before
-                    results = \
-                        pd.concat([results, pd.DataFrame([[filename,
-                                                           'lloyd', k,
-                                                           after, ssq_lloyd]],
-                                                         columns=['dataset',
-                                                                  'method',
-                                                                  'k',
-                                                                  'time',
-                                                                  'ssq'])],
-                                  axis=0)
-                    plot_clustering_results(X, clusters_lloyd, 'lloyd', labels,
-                                            join(plots_dir, filename.split('.')[0] +
-                                                 '_k=' + str(k) + '_lloyd' + '.png'))
-                # BEGIN TEST: MACQUEEN HEURISTIC
-                before = default_timer()
-                try:
-                    clusters_mcq, ssq_mcq = kmns.macqueen_heuristic()
-                except Exception as e:
-                    if verbose:
-                        print(e)
-                    results = \
-                        pd.concat([results, pd.DataFrame([[filename,
-                                                           'macqueen', k,
-                                                           np.nan, np.nan]],
-                                                         columns=['dataset',
-                                                                  'method',
-                                                                  'k',
-                                                                  'time',
-                                                                  'ssq'])],
-                                  axis=0)
-                    continue
-                else:
-                    after = default_timer() - before
-                    results = \
-                        pd.concat([results, pd.DataFrame([[filename,
-                                                           'macqueen', k,
-                                                           after, ssq_mcq]],
-                                                         columns=['dataset',
-                                                                  'method',
-                                                                  'k',
-                                                                  'time',
-                                                                  'ssq'])],
-                                  axis=0)
-                    plot_clustering_results(X, clusters_mcq, 'macqueen',
-                                            labels,
-                                            join(plots_dir, filename.split('.')[0] +
-                                                 '_k=' + str(k) + '_macqueen' + '.png'))
-                # BEGIN TEST: K-FURTHEST HEURISTIC
-                before = default_timer()
-                try:
-                    clusters_kfu, ssq_kfu = kmns.k_furthest_initial_heuristic()
-                except Exception as e:
-                    if verbose:
-                        print(e)
-                    results = \
-                        pd.concat([results, pd.DataFrame([[filename,
-                                                           'k-furthest', k,
-                                                           np.nan, np.nan]],
-                                                         columns=['dataset',
-                                                                  'method',
-                                                                  'k',
-                                                                  'time',
-                                                                  'ssq'])],
-                                  axis=0)
-                    continue
-                else:
-                    after = default_timer() - before
-                    results = \
-                        pd.concat([results, pd.DataFrame([[filename,
-                                                           'k-furthest', k,
-                                                           after, ssq_kfu]],
-                                                         columns=['dataset',
-                                                                  'method',
-                                                                  'k',
-                                                                  'time',
-                                                                  'ssq'])],
-                                  axis=0)
-                    plot_clustering_results(X, clusters_kfu, 'k-furthest',
-                                            labels,
-                                            join(plots_dir, filename.split('.')[0] +
-                                                 '_k=' + str(k) + '_k-furthest' + '.png'))
-                # BEGIN TEST: K-POPULAR HEURISTIC
-                before = default_timer()
-                try:
-                    clusters_kpp, ssq_kpp = kmns.k_popular_initial_heuristic()
-                except Exception as e:
-                    if verbose:
-                        print(e)
-                    results = \
-                        pd.concat([results, pd.DataFrame([[filename,
-                                                           'k-popular', k,
-                                                           np.nan, np.nan]],
-                                                         columns=['dataset',
-                                                                  'method',
-                                                                  'k',
-                                                                  'time',
-                                                                  'ssq'])],
-                                  axis=0)
-                    continue
-                else:
-                    after = default_timer() - before
-                    results = \
-                        pd.concat([results, pd.DataFrame([[filename,
-                                                           'k-popular', k,
-                                                           after, ssq_kpp]],
-                                                         columns=['dataset',
-                                                                  'method',
-                                                                  'k',
-                                                                  'time',
-                                                                  'ssq'])],
-                                  axis=0)
-                    plot_clustering_results(X, clusters_kpp, 'k-popular',
-                                            labels,
-                                            join(plots_dir, filename.split('.')[0] +
-                                                 '_k=' + str(k) + '_k-popular' + '.png'))
+                for random_seed in range(30):
+                    kmns = KMeans(X, random_seed, k, verbose)
+                    heuristics_list = \
+                        [(kmns.lloyd_heuristic, 'lloyd'),
+                         # (kmns.macqueen_heuristic, 'macqueen'),
+                         (kmns.k_means_plus_plus, 'kmeans++'),
+                         (kmns.k_furthest_initial_heuristic, 'k-furthest'),
+                         (kmns.k_popular_initial_heuristic, 'k-popular')]
+                    for heuristic, h_name in heuristics_list:
+                        success, result = \
+                            run_heuristic_on_dataset(heuristic, X, k,
+                                                     h_name, filename,
+                                                     plots_dir, labels,
+                                                     random_seed, verbose)
+                        if verbose:
+                            print ('result shape: ', result.shape)
+                        results = pd.concat([results, result], axis=0)
+                        if verbose:
+                            print ('total results shape: ', results.shape)
+            # Removed polluting code
             results.to_csv(join(outdir, filename.split('.')[0] + '_' +
                                 'results.csv'),
                            index=False)
@@ -188,7 +110,7 @@ if __name__ == "__main__":
     parser.add_argument('-o', '--output', type=str,
                         default='testresults', help='output directory')
     parser.add_argument('-s', '--source', type=str,
-                        default='dataset/real', help='input directory')
+                        default='dataset/synthetic', help='input directory')
     parser.add_argument('--verbose', action='store_true', default=False,
                         help='increase output verbosity')
     args = parser.parse_args()
